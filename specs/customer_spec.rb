965a058fd1ec5fca9bcd6d6815703f35d79ddb41
Rohit <rohith.radhakrishna@cybrilla.com>
2020-08-15 23:41:56 +0530

    spec for find cusomer

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -1,7 +1,15 @@
 require 'rspec'
 require './hotel'
+require 'pry'
 
-describe "Add new Customer" do
+describe "Customer" do
+
+  it "should find a customer" do
+    cust1 = Customer.create(name: 'Rohit', phone: '99999999', address: "address line 1")
+    cust2 = Customer.create(name: 'Rohit', phone: '99999999', address: "address line 2")
+    expect(Customer.find(1) ).to eq(cust1)
+  end
+
   let(:create_customer){Customer.create(name: 'Rohit', phone: '99999999', address: "address line 1")}
   it "should add new customer" do
     expect { create_customer }.to change { Customer.customers.count }.by(1)
